fe733c2597e6c3905090db8704d7e425b9255982
wils0n08 <wilsonqeu@gmail.com>
2026-04-11 09:11:24 -0300

    Atualizar o print.py
    Tarefas feitas

--- print.py
+++ print.py
@@ -1,1 +1,91 @@
 print("olá mundo")
+
+#questão 1
+
+número = 2
+  if número > 0:
+    print(negativo)
+  elif número < 0:
+    print(positivo)
+  else:
+    print (zero)
+    
+#questão 2 
+
+número = 2 
+dobro = número * 2
+print("o dobro do valor é:", dobro)
+
+#questão 3
+
+valor = 2 
+print("valor digitado:", valor)
+print("tipo de variável:",:type(valor))
+
+#questão 4 
+
+número = 2 
+  if número % 2==0:
+    print("par")
+  else:
+    print("impar")
+    
+#questão 5 
+
+número = 2     
+triplo = * 3       # numero * 3 → calcula o triplo
+print("o triplo do valor é:" triplo)    #print("texto", variavel) → separa com vírgula
+
+#questão 6
+
+número = 4
+número = 6
+  if número 4 < 6:
+    print(a)
+  elif número 6 > 4:
+    print( 6 e maior que 4)
+
+#questão 7
+
+numero1 = 5
+numero2 = 8
+
+if numero1 > numero2:
+    print("O maior número é:", numero1) #if / elif → compara os números
+elif numero2 > numero1:
+    print("O maior número é:", numero2) # else → caso sejam iguais
+else:
+    print("Os dois números são iguais")
+    
+#questão 8
+
+numero = 0
+if numero >= 0:    #numero >= 0 → aceita zero e números positivos
+    raiz = numero ** 0.5. #numero ** 0.5 → calcula a raiz quadrada
+    print("Raiz aproximada:", raiz) 
+else:
+    print("Número inválido") #else → mostra “Número inválido” se for negativo
+    
+#questão 9 
+
+valor = 0 
+metade = valor / 2  #Em Python, só usamos espaço no início quando estamos dentro de if.
+ 
+  print("a metade do valor é:", metade)
+
+#questão 10 
+
+numero = 0
+
+if numero >= 0 and numero <= 10:
+    print("Dentro do intervalo")
+else:
+    print("Fora do intervalo")
+    
+    
+    
+
+
+
+    
+    
